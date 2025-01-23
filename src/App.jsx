12897226bb9da004640c52c65d4b16ebebee6457
YTPe4ka <saport202@gmail.com>
2025-01-24 00:18:51 +0500

    end

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes';
 import { AuthProvider } from './context/AuthContext';
-
+import './App.css'
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
